2ad7cc55cd90e10c69dad84b1b00e60ddc86a7cc
nikita <nikitakuzmenko1107@gmail.com>
2022-11-25 19:45:40 +0100

    get implemented

--- src/routes/register.route.js
+++ src/routes/register.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controllers/user.controller');
+const { register, list } = require('../controllers/user.controller');
 const { validateData } = require('../middleware/validateData.mid');
 const { userSchema } = require('../models/user.schema');
 
 router.post('/users', validateData(userSchema, 'body'), register);
+router.get('/users', validateData(userSchema, 'body'), list);
 
 module.exports = router;
