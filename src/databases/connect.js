22556cb84cee3b2062102243b4d477c580cd9c91
nikita <nikitakuzmenko1107@gmail.com>
2022-12-02 17:10:45 +0100

    yaww

--- src/databases/connect.js
+++ src/databases/connect.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 module.exports.createConnection = async () => {
     try {
         await mongoose.connect(
-        process.env.DB_URL,
-        { useNewUrlParser: true, useUnifiedTopology: true }
+            process.env.DB_URL,
+            { useNewUrlParser: true, useUnifiedTopology: true }
         )
         mongoose.connection.on('error', (error) => console.log(`ERROR Connection was interrupted: ${error}`))
     } catch (error) {
